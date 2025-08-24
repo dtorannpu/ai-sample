5be0580bf8ede00d7764b5a64f654f55f4955a9f
dtorannpu <dtorannpu@gmail.com>
2025-08-24 12:07:19 +0900

    Add `google-genai` and `langsmith` dependencies, update `main.py` to use `langsmith` client for simpler prompt execution.

--- src/ai_sample/main.py
+++ src/ai_sample/main.py
@@ -1,19 +1,10 @@
-import os
-import getpass
-from langchain.chat_models import init_chat_model
-
+from langsmith import Client
 
 def main():
-    if not os.environ.get("GOOGLE_API_KEY"):
-        os.environ["GOOGLE_API_KEY"] = getpass.getpass(
-            "Enter API key for Google Gemini: "
-        )
-
-    model = init_chat_model("gemini-2.5-flash", model_provider="google_genai")\
-
-    m = model.invoke("Hello, world!")
-
-    print(m.text())
+    client = Client()
+    chain = client.pull_prompt("sample", include_model=True)
+    response = chain.invoke({})
+    print(response.text())
 
 
 if __name__ == "__main__":
