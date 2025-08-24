2c371bdaf18be8bbf1b35cc5602d98ff01dbcb90
dtorannpu <dtorannpu@gmail.com>
2025-08-24 12:25:04 +0900

    Add docstring to `main` function in `main.py` for clarity.

--- src/ai_sample/main.py
+++ src/ai_sample/main.py
@@ -1,6 +1,9 @@
 from langsmith import Client
 
+
 def main():
+    """Langsmithサンプル."""
+
     client = Client()
     chain = client.pull_prompt("sample", include_model=True)
     response = chain.invoke({})
